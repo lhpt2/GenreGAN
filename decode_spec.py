e86fd4ae4bdac494879f3d607429468b218be04f
Lucas Haupt <lucas.haupt@t-online.de>
2023-08-17 12:42:23 +0200

    v1.0 of GenreGAN

---
+++ decode_spec.py
@@ -0,0 +1,23 @@
+import sys
+
+import matplotlib.pyplot
+import numpy
+import os
+import soundfile
+
+from constants import hop
+from preprocessing import db_spec_to_wave
+import tensorflow as tf
+
+if len(sys.argv) < 3:
+    raise Exception("Give filename and samplerate as arguments")
+
+name = sys.argv[1]
+sr = int(sys.argv[2])
+
+spec = numpy.load(sys.argv[1])
+spec = numpy.squeeze(spec)
+wv = db_spec_to_wave(spec)
+filename = os.path.splitext(os.path.basename(sys.argv[1]))[0]
+soundfile.write(f"{filename}.wav", wv, sr)
+print(f"Audio written to {filename}.wav")
---
+++ plotloss.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+
+# 0 - epoch, 1 - dloss, 2 - dreward, 3 - gloss, 4 - idloss, 5 - learning rate
+
+csvdata = pd.read_csv(sys.argv[1], header=None)
+
+num = csvdata[0][1:-1]
+dloss = csvdata[1][1:-1]
+gloss = csvdata[3][1:-1]
+
+print(num)
+#print(dloss)
+#print(gloss)
+
+
+fig, ax = plt.subplots(figsize=(100, 50))
+ax.plot(dloss, label="Discriminator Loss")
+ax.plot(gloss, label="Generator Loss")
+plt.legend()
+plt.show()
