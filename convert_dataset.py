e89b1dd76619efed72a7552c5b8bb5dacd74a0bd
Lucas Haupt <lucas.haupt@t-online.de>
2023-10-17 00:11:05 +0200

    fix call to load_wv function

--- convert_dataset.py
+++ convert_dataset.py
@@ -1,7 +1,6 @@
 from tools.dataset_processing import load_wv_array_splitseconds_checked, construct_save_ds
 
-
-ids, olist, rlist = load_wv_array_splitseconds_checked("./dsSimple/train_o")
+ids, olist, rlist = load_wv_array_splitseconds_checked("./dsSimple/train")
 construct_save_ds(ids, olist, rlist, "dsSimple_train")
-ids, val_o, val_r = load_wv_array_splitseconds_checked("./dsSimple/val_o")
+ids, val_o, val_r = load_wv_array_splitseconds_checked("./dsSimple/val")
 construct_save_ds(ids, val_o, val_r, "dsSimple_val")
