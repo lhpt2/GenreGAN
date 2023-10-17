cf9c7eed8c1c69a028b349dc60dc55045af13aa6
Lucas Haupt <lucas.haupt@t-online.de>
2023-10-17 02:12:24 +0200

    convert dataset updated

--- convert_dataset.py
+++ convert_dataset.py
@@ -1,6 +1,7 @@
+from MAINCONFIG import *
 from tools.dataset_processing import load_wv_array_splitseconds_checked, construct_save_ds
 
-ids, olist, rlist = load_wv_array_splitseconds_checked("./dsSimple/train")
+ids, olist, rlist = load_wv_array_splitseconds_checked("../dsSimple/train")
 construct_save_ds(ids, olist, rlist, "dsSimple_train")
-ids, val_o, val_r = load_wv_array_splitseconds_checked("./dsSimple/val")
+ids, val_o, val_r = load_wv_array_splitseconds_checked("../dsSimple/val")
 construct_save_ds(ids, val_o, val_r, "dsSimple_val")
