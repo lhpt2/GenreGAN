d58b6f35e8727a91721241cb3113e8c1ddc6c6c0
Lucas Haupt <lucas.haupt@t-online.de>
2023-10-27 11:40:50 +0200

    loss plotting script adjusted (different nets generations have different logging format)

--- plotloss.py
+++ plotloss.py
@@ -3,13 +3,17 @@ import pandas as pd
 import sys
 
 # 0 - epoch, 1 - dloss, 2 - dreward, 3 - gloss, 4 - idloss, 5 - learning rate
+# 0 - epoch, 1 - dloss_fake, 2 - dloss_real, 3 - gloss, 4 - idloss, 5 - learning rate
 
-csvdata = pd.read_csv(sys.argv[1], header=None)
+# 0 epoch, 1 - dloss, 2- drloss, 3 - dfloss, 4 - gloss, 5 - sloss, 6 - idloss, 7 - vloss, 8 - lr
+
+csvdata = pd.read_csv(sys.argv[1], header=0)
 
 num = csvdata[0][1:-1]
 dloss = csvdata[1][1:-1]
 gloss = csvdata[3][1:-1]
-
+#gloss = csvdata[4][1:-1]
+vloss = csvdata[7][1:-1]
 
 print(num)
 #print(dloss)
